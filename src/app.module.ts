b7e968e6bb56eeba06b35d94c160fcd4fdd93b7a
Sergio Lepore <sergio.d.lepore@gmail.com>
2024-02-29 16:01:09 -0300

    New: add `developers` module
    - nest g resource developers
    - REST API resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
+import { DevelopersModule } from './developers/developers.module';
 
 @Module({
-  imports: [TestModule],
+  imports: [TestModule, DevelopersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/developers/developers.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@nestjs/common';
+import { CreateDeveloperDto } from './dto/create-developer.dto';
+import { UpdateDeveloperDto } from './dto/update-developer.dto';
+import { Developer } from './entities/developer.entity';
+
+@Injectable()
+export class DevelopersService {
+  private readonly developersDB: Developer[] = [];
+
+  create(createDeveloperDto: CreateDeveloperDto): Developer {
+    const newDeveloper = new Developer(
+      this.developersDB.length + 1,
+      createDeveloperDto.name,
+      createDeveloperDto.email,
+    );
+
+    this.developersDB.push(newDeveloper);
+
+    return newDeveloper;
+  }
+
+  findAll(): Developer[] {
+    return this.developersDB;
+  }
+
+  findOne(id: number): Developer | undefined {
+    return this.developersDB.find((developer) => developer.id === id);
+  }
+
+  update(
+    id: number,
+    updateDeveloperDto: UpdateDeveloperDto,
+  ): Developer | undefined {
+    const developer = this.findOne(id);
+
+    if (developer) {
+      developer.name = updateDeveloperDto.name;
+      developer.email = updateDeveloperDto.email;
+    }
+
+    return developer;
+  }
+
+  remove(id: number) {
+    const index = this.developersDB.findIndex(
+      (developer) => developer.id === id,
+    );
+
+    if (index !== -1) {
+      this.developersDB.splice(index, 1);
+    }
+  }
+}
